e11a2ace2c8436dd7034725cdac364fe2f689361
VaderSteve76 <alverson.s@yahoo.com>
2020-05-22 18:07:37 -0400

    Added key up & down events

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ let score = 0;
 const brickRowCount = 9;
 const brickColumnCount = 5;
 
+// Keydown event
+function keyDown(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    paddle.dx = paddle.speed;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+// Keyup event
+function keyUp(e) {
+  if (
+    e.key === 'Right' ||
+    e.key === 'ArrowRight' ||
+    e.key === 'Left' ||
+    e.key === 'ArrowLeft'
+  ) {
+    paddle.dx = 0;
+  }
+}
 
 // Keyboard event handlers
 document.addEventListener('keydown', keyDown);
