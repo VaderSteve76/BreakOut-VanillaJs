1c82b0182d76be4615b41b7738aa890badf43d41
VaderSteve76 <alverson.s@yahoo.com>
2020-05-22 18:04:08 -0400

    Added getElements & Event Listeners

---
+++ script.js
@@ -0,0 +1,19 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
